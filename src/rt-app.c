8126b4630c9f944ea311bd7f706c958063e802ba
Juri Lelli <juri.lelli@arm.com>
2016-02-13 18:40:00 +0000

    fix warnings regarding pthread lib
    Fix a couple of warnings regarding pthread lib by using appropriate
    include and define.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- src/rt-app.c
+++ src/rt-app.c
@@ -18,6 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#define _GNU_SOURCE
+#include <pthread.h>
 #include <fcntl.h>
 #include "rt-app.h"
 #include "rt-app_utils.h"
